26358fd52d880654fb8d77abffef32bcbb45729a
Hernan Ariel De Luca <hadeluca@gmail.com>
2018-01-24 01:30:03 +0100

    aggiungere utente

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -69,4 +69,68 @@ class UsersController extends Controller
 
         return $listManager->publish();
     }
+
+    public function create(Request $request) {
+
+        return parent::create($request);
+    }
+
+    public function edit(Request $request, $id) {
+
+        return parent::edit($request, $id);
+    }
+
+    public function buildEditManager(Request $request, $id = null) {
+
+        $editManager = editStart([
+            'title' => 'Utente',
+            'subTitle' => 'Elenco degli Utenti',
+            'objName' => 'App\User',
+            'objId' => $id,
+            'input' => $request->all(),
+        ])->addLabel([
+            'label' => 'Definizioni generali'
+        ])->addCp('bl','block',[
+            'label' => 'Definizioni generali',
+            'icon' => 'fa fa-database',
+        ])->addCp('fl','text',[
+            'label' => 'Nome',
+            'field' => 'name',
+            'validation' => 'required',
+            'validationMess' => [
+                'required' => 'Il campo nome è obbligatorio',
+            ],
+        ])->addCp('fl','text',[
+            'label' => 'Cognome',
+            'field' => 'surname',
+            'validation' => 'required',
+            'validationMess' => [
+                'required' => 'Il campo nome è obbligatorio',
+            ],
+        ])->addCp('fl','date',[
+            'label' => 'Data di Nascita',
+            'field' => 'birthdate',
+            'validation' => 'required',
+            'validationMess' => [
+                'required' => 'Il campo nome è obbligatorio',
+            ],
+        ])->addCp('fl','text',[
+            'label' => 'Comune di nascita',
+            'field' => 'comune_nasc',
+            'validation' => 'required',
+            'validationMess' => [
+                'required' => 'Il campo nome è obbligatorio',
+            ],
+        ])->addCp('fl','text',[
+            'label' => 'Codice Fiscale',
+            'field' => 'cod_fisc',
+            'validation' => 'required',
+            'validationMess' => [
+                'required' => 'Il campo nome è obbligatorio',
+            ],
+        ]);
+
+
+        return $editManager;
+    }
 }
