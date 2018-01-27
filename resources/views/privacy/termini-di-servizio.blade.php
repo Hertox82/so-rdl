a4ce591aadc1c96f9e7ddfa71ecdfc027e67241f
Darkseal <games@myst.it>
2018-01-27 03:16:08 +0100

    Changed cfCheck and Register & ToS Views
    - Replaced Fiscal Code WS call (async) with RegEx check (sync) to
    prevent future HSTS errors
    
    - Added ToS content
    
    - Minor changes to the Privacy & ToS disclaimer.
    
    - Added VS2017 files to .gitignore

--- resources/views/privacy/termini-di-servizio.blade.php
+++ resources/views/privacy/termini-di-servizio.blade.php
@@ -13,9 +13,70 @@
     </style>
     <div class="row">
         <div class="col-md-12">
-            <div class="alert alert-info">
-                <strong>TODO!</strong>
-            </div>
+
+			<h1>Termini di Servizio</h1>
+
+			Condizioni di utilizzo dei servizi del sito rdlm5s.it
+
+			<h2>Regole Generali e definizioni</h2>
+			Utilizzando i servizi connessi al sito rdlm5s.it (di seguito indicate come "i servizi" o "il servizio"), l'utente è tenuto a rispettare Ie condizioni e i termini di utilizzo qui di seguito precisate. Queste condizioni riguardano i proprietari e i gestori dei contenuti del sito stesso e dei relativi servizi: per maggiori informazioni sulla proprietà e sulla gestione del sito, consultare la privacy policy.
+
+I gestori del servizio si riservano la facoltà di modificare, aggiungere o eliminare parti di queste condizioni, portandone a conoscenza gli interessati attraverso la pubblicazione delle modifiche nel sito o attraverso la posta elettronica. Ogni lettore è tenuto a verificare periodicamente queste condizioni per accertarsi di eventuali modifiche intervenute successivamente all’ultima consultazione del sito. In ogni caso l’utilizzo del sito e dei suoi servizi comporta l’accettazione dei cambiamenti nel frattempo intervenuti. Qualora le condizioni di servizio non siano accettabili, l'utente può richiedere in ogni momento l'eliminazione del proprio account scrivendo a info@rdlm5s.it, fermo restando che la prosecuzione dell’utilizzo dei servizi comporta l’accettazione delle nuove condizioni.
+
+I gestori del servizio si riservano la facoltà di modificare, sospendere, interrompere anche solo parzialmente i servizi, inclusa l’accessibilità al database o ai contenuti. I gestori del servizio si riservano inoltre la facoltà di introdurre limitazioni di accesso o di servizi, in tutto o in parte, senza preavviso e senza assumersi responsabilità per tale limitazione di servizio.
+
+
+			<h1>Informativa sull'utilizzo dei Cookie</h1>
+
+			Per far funzionare bene questo sito invieremo al tuo browser dei piccoli file di dati che si chiamano "cookies". Si tratta di un procedimento assolutamente normale compiuto dalla maggior parte dei siti, portali e servizi web allo scopo di migliorare l'esperienza di navigazione dei propri utenti.
+
+			<h2 class="subtitle3">Cosa sono i cookies?</h2>
+
+			Un cookie è un piccolo file di testo che i siti salvano sul tuo computer o dispositivo mobile mentre li visiti. Grazie ai cookies il sito ricorda le tue azioni e preferenze (per es. login, lingua, dimensioni dei caratteri e altre impostazioni di visualizzazione) in modo che tu non debba reinserirle quando torni sul sito o navighi da una pagina all'altra.
+
+			<h2 class="subtitle3">Come utilizziamo i cookies?</h2>
+
+			In alcune pagine utilizziamo i cookies per ricordare:
+
+			<ul>
+				<li>le preferenze di visualizzazione, per es. le impostazioni del contrasto o le dimensioni dei caratteri</li>
+				<li>se hai già risposto a un sondaggio pop-up sull'utilità dei contenuti trovati, per evitare di riproportelo</li>
+				<li>se hai autorizzato l'uso dei cookies sul sito.</li>
+			</ul>
+
+			Inoltre, alcuni video inseriti nelle nostre pagine utilizzano un cookie per elaborare statistiche, in modo anonimo, su come sei arrivato sulla pagina e quali video hai visto.
+
+Non è necessario abilitare i cookies perché il sito funzioni, ma farlo migliora la navigazione. È possibile cancellare o bloccare i cookies, però in questo caso alcune funzioni del sito potrebbero non funzionare correttamente.
+
+Le informazioni riguardanti i cookies non sono utilizzate per identificare gli utenti e i dati di navigazione restano sempre sotto il nostro controllo. Questi cookies servono esclusivamente per i fini qui descritti.
+
+			<h2 class="subtitle3">Come controllare i cookies?</h2>
+
+			Puoi controllare e/o verificare i cookies come vuoi – per saperne di più, vai su
+			<a href="http://www.aboutcookies.org/">aboutcookies.org</a>. Puoi cancellare i cookies già presenti nel computer e impostare quasi tutti i browser in modo da bloccarne l'installazione. Se scegli questa opzione, dovrai però modificare manualmente alcune preferenze ogni volta che visiti il sito ed è possibile che alcuni servizi o determinate funzioni non siano disponibili.
+
+			<h2>Altri link utili</h2>
+
+			<ul>
+				<li>
+					<strong>
+						<a href="http://ec.europa.eu/ipg/basics/legal/cookies/index_en.htm" target="_blank">Cookies - European Commission</a>
+					</strong>, il sito di riferimento sulla normativa Europea sull'utilizzo dei Cookie a cura della Commissione Europea.
+				</li>
+				<li>
+					<strong>
+						<a href="http://www.garanteprivacy.it/cookie" target="_blank">Cookie - Garante Privacy</a>
+					</strong>, il sito di riferimento sulla normativa in Italia a cura del Garante della Privacy.
+				</li>
+				<li>
+					<strong>
+						<a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank">HTTP cookie - Wikipedia</a>
+					</strong>, una panoramica su cosa sono e a cosa servono i cookie a cura di Wikipedia.com (
+					<a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank">fai click qui per la versione in lingua italiana</a>).
+				</li>
+			</ul>
+
+
         </div>
     </div>
 @endsection
